4c0fb2223890b814379b108ef9d6e193050e3f1a
y2k <itwisterlx@gmail.com>
2017-05-14 23:57:11 +0300

    refactoring

--- src/domain.ts
+++ src/domain.ts
@@ -19,8 +19,10 @@ export interface PostsStates {
 
 module PostsFunctions {
 
-    export const reset = (state: PostsStates | null): PostsStates =>
-        (state && { ...state, next: null }) || { preloaded: [], posts: [], old: [], next: null }
+    export function mergeToClear(state: PostsStates | null): PostsStates {
+        if (state == null) return { preloaded: [], posts: [], old: [], next: null }
+        return { preloaded: [], posts: [], old: state.posts.concat(state.old), next: null }
+    }
 
     export function merge(state: PostsStates, posts: Post[], next: number): PostsStates {
         const actual = state.posts
@@ -36,19 +38,19 @@ export module Loader {
     export const debugReset = () => AS.clear()
 
     export async function loadFromStorage(source: Source): Promise<PostsStates> {
-        return PostsFunctions.reset(JSON.parse(await AS.getItem("state")))
+        return PostsFunctions.mergeToClear(JSON.parse(await AS.getItem("state")))
     }
 
     export async function preload(source: Source): Promise<PostsStates> {
         const state = await loadFromStorage(source)
-        const resp = await request<PostResponse>(Domain.postsUrl(source, state.next), "posts")
+        const resp = await request<PostResponse>(Domain.postsUrl(source, state.next))
         const newState = PostsFunctions.merge(state, resp.posts, resp.nextPage)
         await AS.setItem("state", JSON.stringify(newState))
         return newState
     }
 
     export async function loadNext(state: PostsStates, source: Source): Promise<PostsStates> {
-        const resp = await request<PostResponse>(Domain.postsUrl(source, state.next), "posts")
+        const resp = await request<PostResponse>(Domain.postsUrl(source, state.next))
         const newState = PostsFunctions.merge(state, resp.posts, resp.nextPage)
         await AS.setItem("state", JSON.stringify(newState))
         return newState
@@ -59,13 +61,13 @@ export module Loader {
      */
 
     export const loadProfile = (name: string): Promise<Profile> =>
-        request(Domain.profileUrl(name), "profile")
+        request(Domain.profileUrl(name))
 
     export const postDescription = (id: number): Promise<Post> =>
-        request(Domain.postDetailsUrl(id), "post")
+        request(Domain.postDetailsUrl(id))
 
-    function request<T>(path: string, parseApi: string): Promise<T> {
-        return fetch(`http://joyreactor.cc${path}`)
+    function request<T>(api: ParserApi): Promise<T> {
+        return fetch(`http://joyreactor.cc${api.path}`)
             .then(x => x.text())
             .then(x => {
                 const form = new FormData()
@@ -77,26 +79,30 @@ export module Loader {
                     body: form
                 }
 
-                return fetch(`http://212.47.229.214:4567/${parseApi}`, request)
+                return fetch(`http://212.47.229.214:4567/${api.parser}`, request)
             })
             .then(x => x.json())
     }
 }
 
+interface ParserApi { readonly path: string, readonly parser: string }
+
 export namespace Domain {
 
-    export const profileUrl = (name: string) => `/user/${encodeURIComponent(name)}`
-    export const postDetailsUrl = (post: number) => `/post/${post}`
+    export const profileUrl = (name: string): ParserApi =>
+        ({ path: `/user/${encodeURIComponent(name)}`, parser: "profile" })
+    export const postDetailsUrl = (post: number): ParserApi =>
+        ({ path: `/post/${post}`, parser: "post" })
 
-    export function postsUrl(tag: Source, page: number | null) {
+    export function postsUrl(tag: Source, page: number | null): ParserApi {
         if (page == null) // TODO: объединить логику
             switch (tag.kind) {
-                case "feed": return "/"
-                case "tags": return `/tag/${encodeURIComponent(tag.name)}`
+                case "feed": return { path: "/", parser: "posts" }
+                case "tags": return { path: `/tag/${encodeURIComponent(tag.name)}`, parser: "posts" }
             }
         switch (tag.kind) {
-            case "feed": return `/${page}`
-            case "tags": return `/tag/${encodeURIComponent(tag.name)}/${page}`
+            case "feed": return { path: `/${page}`, parser: "posts" }
+            case "tags": return { path: `/tag/${encodeURIComponent(tag.name)}/${page}`, parser: "posts" }
         }
     }
 
