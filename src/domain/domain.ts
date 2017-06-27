81882a9aaa30641eba4e78c7a6594f899f39bae4
y2k <itwisterlx@gmail.com>
2017-06-27 12:42:46 +0300

    refactoring

--- src/domain/domain.ts
+++ src/domain/domain.ts
@@ -1,57 +1,7 @@
 import { AsyncStorage as AS, Dimensions } from "react-native"
 import { Post, Source, Posts_, Profile, Attachment } from "types"
 
-// export interface Profile {
-//     userName: string,
-//     userImage: Attachment,
-//     rating: number,
-//     stars: number,
-//     progressToNewStar: number
-// }
-// export interface Attachment { url: string, aspect: number }
-// export interface Comment { text: string, image: Attachment, rating: number }
-// export interface Post {
-//     id: number,
-//     userName: string,
-//     userImage: Attachment,
-//     rating: number,
-//     created: number,
-//     title: string,
-//     image: Attachment,
-//     comments: Comment[]
-// }
-
 interface PostResponse { posts: Post[], nextPage: number }
-
-// export interface TagSource { kind: "tags", name: string }
-// export interface FeedSource { kind: "feed" }
-// export type Source = FeedSource | TagSource
-
-// export interface PostsFromCache {
-//     readonly kind: "PostsFromCache",
-//     readonly source: Source,
-//     readonly posts: Post[],
-// }
-
-// export interface PostsFromCachedAndWeb {
-//     readonly kind: "PostsFromCachedAndWeb",
-//     readonly source: Source,
-//     readonly posts: Post[],
-//     readonly preloadedPosts: Post[],
-//     readonly next: number | null,
-// }
-
-// export interface PostsWithNextPage {
-//     readonly kind: "PostsWithNextPage",
-//     readonly source: Source,
-//     readonly posts: Post[],
-//     readonly oldPosts: Post[],
-//     readonly next: number | null,
-// }
-
-// export interface PostsError { readonly kind: "PostsError" }
-
-// export type Posts_ = PostsFromCache | PostsFromCachedAndWeb | PostsWithNextPage | PostsError
 
 module PostsFunctions {
 
@@ -66,7 +16,6 @@ module PostsFunctions {
 }
 
 export module Loader {
-
 
     export const preload = async (source: Source): Promise<Posts_> => {
         // JSON.parse(await AS.getItem("state"))
