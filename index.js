c1d5c14ac39aa91d621110edbfe226842466f3de
y2k <itwisterlx@gmail.com>
2019-07-21 17:13:50 +0300

    fix rn build

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-// import 'es6-symbol/implement';
-
-// import { PostComponent } from './out/App'
-// import { AppRegistry } from 'react-native';
-
-// AppRegistry.registerComponent('joyreact', () => PostComponent);
-
-
 import * as coreJs from "core-js/shim";
 
 import { AppRegistry } from 'react-native';
